4e51401fc98511c935414e11fb30b324840fd720
Meet-Sabhani <meetsabhani18@gmail.com>
2023-12-05 18:55:11 +0530

    second

--- script.js
+++ script.js
@@ -3,9 +3,4 @@ let ul = document.querySelector(".nav-links");
 
 menu.addEventListener("click", () => {
   ul.classList.toggle("showData");
-  ul.className === "showData"
-    ? (menu.className = "fas fa-times")
-    : (menu.className = "fas fa-bars");
 });
-
-console.log('kkk')
