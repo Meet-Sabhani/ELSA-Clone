e28e78a020b7cd7c1b7026777fbdb28d72ba2b86
Meet-Sabhani <meetsabhani18@gmail.com>
2023-12-07 16:34:02 +0530

    display

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let showData = document.querySelector(".showData");
 menu.addEventListener("click", () => {
   ul.classList.toggle("showData");
 });
+
+const allDrops = document.querySelectorAll(".Drop-Down");
+
+const setOpenMenu = () => {
+  allDrops.forEach((drop) => {
+    drop.style.display = drop.style.display === "block" ? "none" : "block";
+  });
+};
