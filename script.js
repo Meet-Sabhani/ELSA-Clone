df74ba7c87cefcb9f49e7380ee045571f8806fe8
Meet-Sabhani <meetsabhani18@gmail.com>
2023-12-07 16:07:57 +0530

    keyfram

--- script.js
+++ script.js
@@ -4,5 +4,4 @@ let showData = document.querySelector(".showData");
 
 menu.addEventListener("click", () => {
   ul.classList.toggle("showData");
-  // showData.style.hight === '0' ? showData.style.hight = '170px' : showData.style.hight = '0'
 });
