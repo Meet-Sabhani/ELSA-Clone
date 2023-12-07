6911321b16034508a270c4fe649594af56af9f93
Meet-Sabhani <meetsabhani18@gmail.com>
2023-12-07 09:36:41 +0530

    nav

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let menu = document.getElementById("menu-icon");
 let ul = document.querySelector(".nav-links");
+let showData = document.querySelector(".showData")
 
 menu.addEventListener("click", () => {
   ul.classList.toggle("showData");
